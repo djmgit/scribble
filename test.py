efc348d0a7674b6ff19f10243a230ad4a1ed1bd8
djmgit <djmdeveloper060796@gmail.com>
2018-05-28 15:25:17 +0530

    Updated test file

--- test.py
+++ test.py
@@ -12,7 +12,14 @@ s.set_params("tetsing title3", "testing body3", "", "")
 print (s.save_note())
 '''
 
+'''
 from view_all import View_all
 s = View_all()
 s.set_params()
 print (s.view_all())
+'''
+
+from view_by_id import View_by_id
+s = View_by_id()
+s.set_params(5)
+print (s.view_by_id())
---
+++ view_by_id.py
@@ -0,0 +1,64 @@
+from statuses import *
+import getpass
+from api_call import Api_call
+from token_manager import Token_Manager
+from tabulate import tabulate
+
+class View_by_id:
+
+	def __init__(self):
+		self.token = ""
+		self.path = "note_by_id"
+		self.note_id = ""
+
+	def set_params(self, note_id):
+		token = Token_Manager().get_token()
+		if token == 0:
+			exit()
+
+		self.token = token
+		self.note_id = note_id
+
+	def view_by_id(self):
+		headers = {}
+		payload = {}
+
+		headers["Authorization"] = self.token
+
+		payload["note_id"] = self.note_id
+
+		print ("Retrieving your note...")
+
+		resp = Api_call().apicall(payload, headers, self.path)
+
+		if resp.get("status") == TOKEN_NOT_FOUND or resp.get("status") == NOT_LOGGED_IN :
+			print ("You are not logged in. Please log in first.")
+			return 0
+
+		if resp.get("status") == NOTE_ID_NOT_FOUND:
+			print ("Please mention a note id")
+			return 0
+
+		if resp.get("status") == ERROR:
+			data = resp.get("data")
+			code = data.get("code")
+			message = data.get("message")
+			print ("{} : {}".format(code, message))
+			return 0
+
+		data = resp.get("data")[0]
+		
+		print ("\n")
+		print ("#{} {}".format(data["note_id"], data["note_title"]))
+		print ("====================================================================")
+		print ("\n")
+		print (data["note_body"])
+		print ("\n")
+		if data.get("keywords") and data.get("keywords") != "":
+			print ("Keywords : {}".format(data["keywords"]))
+			print ("\n")
+		if data.get("category") and data.get("category") != "":
+			print ("category : {}".format(data["category"]))
+			print ("\n")
+
+		return 1
