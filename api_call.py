ff8e671f5c30911e13736e5e45eef602bbcc33c3
djmgit <djmdeveloper060796@gmail.com>
2018-05-30 10:00:22 +0530

    Added try catch to tackle the situation when hasura cluster is sleeping

--- api_call.py
+++ api_call.py
@@ -10,10 +10,24 @@ class Api_call:
 
 	def apicall(self, payload, headers, path):
 		self.headers["Content-Type"] = "application/json"
+		json_data = ""
 
 		if headers.get("Authorization"):
 			self.headers["Authorization"] = headers.get("Authorization")
 
-		resp = requests.request("POST", self.API_BASE_URL + '/' + path, data=json.dumps(payload), headers=self.headers)
+		'''
+		this is a hack. This application uses free version of hasura. The cluster sleeps
+		when not in use for a long time. So if cluster is sleeping, then the api will fail.
+		As a result an exception will be raised (as python will not able to extract any json)
+		and finally the script will be terminated
+		'''
 
-		return resp.json()
+		try:
+			resp = requests.request("POST", self.API_BASE_URL + '/' + path, data=json.dumps(payload), headers=self.headers)
+			json_data = resp.json()
+		except:
+			print ("Scribble is sleeping!")
+			print ("Formally speaking, the hasura cluster is sleeping due to inactivity, please wake it up :P")
+			exit()
+
+		return json_data
