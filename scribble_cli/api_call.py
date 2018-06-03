3a77a6a00875054ffdb333b6f89f9f087c45aa1b
djmgit <djmdeveloper060796@gmail.com>
2018-06-03 11:14:29 +0530

    Added method to wakeup cluster

--- scribble_cli/api_call.py
+++ scribble_cli/api_call.py
@@ -30,6 +30,7 @@ class Api_call:
 		except:
 			self.logger.info("Scribble is sleeping!")
 			print ("Formally speaking, the hasura cluster is sleeping due to inactivity, please wake it up :P")
+			print ("Visit http://auth.exemplification12.hasura-app.io/ui to wake your cluster up")
 			exit()
 
 		return json_data
